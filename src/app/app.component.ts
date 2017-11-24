1f060f0b4c6daff43dea7ecc761e112006f16bcb
rafaesc <rafaesc92@gmail.com>
2017-11-24 15:13:21 -0500

    Added konva example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public configStage = Observable.of({
+    width: 200,
+    height: 200
+  });
+  public configCircle = Observable.of({
+    x: 100,
+    y: 100,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4
+  });
+
+  public handleClick(component) {
+    console.log('Hello Circle', component);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { KonvaModule } from 'ng2-konva';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KonvaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
